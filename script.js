a28ac86eaafbc26d1bf7a3ecc3c2519647bd84bc
jamesjeong94 <jamesjeong94@gmail.com>
2020-02-27 17:13:29 -0500

    added func to parse gtfs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const GtfsRealTimeBindings = require('gtfs-realtime-bindings')
 const request = require('request')
-let key = '282ebf9d15c7efa63bd01707b0ac86fc'
-let orangeLineFeed = `http://datamine.mta.info/mta_esi.php?key=${key}&feed_id=21`
+const apiKey = '282ebf9d15c7efa63bd01707b0ac86fc'
+const orangeLineFeed = `http://datamine.mta.info/mta_esi.php?key=${apiKey}&feed_id=21`
 const requestSettngs = {
     method: 'GET',
     url: orangeLineFeed,
@@ -11,10 +11,11 @@ const requestSettngs = {
 request(requestSettngs, (error, response, body) => {
     if (!error && response.statusCode === 200){
         let feed = GtfsRealTimeBindings.transit_realtime.FeedMessage.decode(body);
+        let position = GtfsRealTimeBindings.transit_realtime
+        console.log(position);
         feed.entity.forEach((entity) => {
-            console.log(entity.trip_update)
-            if (entity.trip_update){
-                console.log(entity.trip_update)
+            if (entity.tripUpdate){
+                //console.log('printing',entity.tripUpdate)
             }
         })
     }
